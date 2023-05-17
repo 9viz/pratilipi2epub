09fc375d7870871b2d9d13fbb3d265112a76f303
Visuwesh <visuweshm@gmail.com>
2023-05-17 11:15:51 +0530

    Finish up the program
    * (SeriesPartResp, getSeriesPart, extractLinks): Check if chapter is
    accessible.
    * (ContentExtra, getContent): Add function to fetch chapter content.
    * (prepareContentOpf): Fix, and allow for files other than HTML.
    * (prepareTocNcx, prepareContainerXml, prepareMimetype): Fix, and make
    them return a byte array.
    * (getSeriesTitle): Add function to fetch the series title.
    * (Zipfile, do): Add function to fetch all chapters and put them in an
    epub file.
    * (main): Make the executable accept series links as arguments.

--- pratilipi2epub.go
+++ pratilipi2epub.go
@@ -1,14 +1,20 @@
+// -*- compile-command: "go build pratilipi2epub.go"; -*-
 package main
 
 import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/go-rod/rod"
+	"golang.org/x/net/html"
 	"io/ioutil"
 	"net/http"
+	nurl "net/url"
 	"strconv"
 	"strings"
 	"time"
+	"os"
+	"archive/zip"
 )
 
 type QueryVarWhere struct {
@@ -99,6 +105,9 @@ type SeriesPartResp struct {
 							Title       string `json:"title"`
 							ReadPageURL string `json:"readPageUrl"`
 							PublishedAt int64  `json:"publishedAt"`
+							PratilipiEarlyAccess struct {
+								IsEarlyAccess bool `json:"isEarlyAccess"`
+							} `json:"pratilipiEarlyAccess"`
 						} `json:"pratilipi"`
 					} `json:"parts"`
 				} `json:"publishedParts"`
@@ -118,6 +127,7 @@ func getSeriesPart(slug string, limit int, cursor int) SeriesPartResp {
 		"title " +
 		"readPageUrl " +
 		"publishedAt " +
+		"pratilipiEarlyAccess { isEarlyAccess } " +
 		"} } } } } }"
 	querydata := Query{
 		"getSeriesPartsPaginatedBySlug",
@@ -161,8 +171,11 @@ func extractLinks(slug string) [][]string {
 		if len(d) != limit {
 			fetchp = false
 		}
-		fmt.Println(fetchp)
 		for _, i := range d {
+			if i.Pratilipi.PratilipiEarlyAccess.IsEarlyAccess {
+				fetchp = false
+				break
+			}
 			i := i.Pratilipi
 			t := time.Unix(i.PublishedAt/1000, 0)
 			t = t.Local()
@@ -175,6 +188,125 @@ func extractLinks(slug string) [][]string {
 		cursor += limit
 	}
 	return url
+}
+
+// Extra data that are needed for the chapter.
+type ContentExtra struct {
+	FileName    string
+	FileId      string
+	FileContent []byte
+	Mimetype    string
+}
+
+// Return the story content with story details stored in DATA.
+// DATA follows the format of return value of extractLinks.
+// BROWSER is the rod Browser object used to fetch the pages.
+// N denotes the chapter's number.
+// The function might return another nested list of the struct
+// ContentExtra.
+// The function returns ("", nil) if there is no div .content-section
+// element could not be found.
+func getContent(browser *rod.Browser, data []string, N int) (string, []ContentExtra) {
+	page := browser.MustPage("https://tamil.pratilipi.com" + data[1])
+	defer page.MustClose()
+
+	page.MustWaitLoad()
+	var contentBuilder strings.Builder
+	var extra []ContentExtra
+
+	// Check if it contains the relevant div section first.
+	if has := page.MustHas("div .book-content"); !has {
+		return "", nil
+	}
+
+	// Preamble.
+	contentBuilder.WriteString(`<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ta">
+  <head>
+    <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+    <title>`)
+	contentBuilder.WriteString(data[0])
+	contentBuilder.WriteString(`</title>
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+  </head>
+  <body>`)
+
+	// First, find the h1 tag.
+	contentBuilder.WriteString("\n")
+	contentBuilder.WriteString(page.MustElement("div .book-content").MustElement("h1").MustHTML())
+	contentBuilder.WriteString("\n\n")
+
+	// The idea of using a tokenizer rather than html.Parser came
+	// from https://zetcode.com/golang/net-html/.
+	contentHtml := page.MustElement("div .content-section").MustHTML()
+	token := html.NewTokenizer(strings.NewReader(contentHtml))
+	dataSuggestion := false
+	imgCounter := 1
+OLoop:
+	for {
+		tk := token.Next()
+		tok := token.Token()
+		switch {
+		case tk == html.ErrorToken:
+			break OLoop
+		case tk == html.StartTagToken && tok.Data == "span":
+			for _, i := range tok.Attr {
+				if i.Key == "data-suggestions" {
+					contentBuilder.WriteString(i.Val)
+					dataSuggestion = true
+					break
+				}
+			}
+			if !dataSuggestion {
+				contentBuilder.WriteString(html.UnescapeString(tok.String()))
+			}
+		case tk == html.EndTagToken && tok.Data == "span":
+			if !dataSuggestion {
+				contentBuilder.WriteString(html.UnescapeString(tok.String()))
+			}
+			dataSuggestion = false
+		// TODO:
+		case tok.Data == "img":
+			imgDone := false
+			for _, i := range tok.Attr {
+				if i.Key == "src" {
+					res, err := page.GetResource(html.UnescapeString(i.Val))
+					if err != nil {
+						break
+					}
+					mimetype := http.DetectContentType(res)
+					// ../Images because src is relative to Text/.
+					contentBuilder.WriteString("<img src='../Images/ch")
+					contentBuilder.WriteString(strconv.Itoa(N))
+					contentBuilder.WriteString("_img")
+					contentBuilder.WriteString(strconv.Itoa(imgCounter))
+					contentBuilder.WriteString("' />")
+					extra = append(extra, ContentExtra{
+						"Images/ch" + strconv.Itoa(N) + "_img" + strconv.Itoa(imgCounter),
+						"ch" + strconv.Itoa(N) + "img" + strconv.Itoa(imgCounter),
+						res,
+						mimetype,
+					})
+					imgCounter += 1
+					imgDone = true
+					break
+				}
+			}
+			if !imgDone {
+				contentBuilder.WriteString(html.UnescapeString(tok.String()))
+			}
+		default:
+			contentBuilder.WriteString(html.UnescapeString(tok.String()))
+		}
+	}
+
+	// End preamble.
+	contentBuilder.WriteString(`</body>
+</html>
+`)
+
+	return contentBuilder.String(), extra
 }
 
 // >>>>>>>>>> EPUB SECTION <<<<<<<<<<<<<
@@ -192,18 +324,22 @@ func extractLinks(slug string) [][]string {
 // Opening the epub archive yourself will also give you a good idea of
 // what needs to be done.
 
-// Prepare the content.opf file for the series.  AUTHOR is the author
-// of the series, TITLE is the name of the series, IDENTIFIER is the value of
-// unique-identifier for the series, FILES is a list of [ FTITLE,
-// FILENAME ] where FTITLE is the title of the chapter, and FILENAME
-// is the filename of the chapter.
+// Return the file contents of the content.opf file for the series.
+// AUTHOR is the author of the series, TITLE is the name of the
+// series, IDENTIFIER is the value of unique-identifier for the
+// series, FILES is a list of [ FTITLE, FILENAME ] where FTITLE is the
+// title of the chapter, and FILENAME is the filename of the chapter.
+// FILES may also contain [ FTITLE, FILENAME, MIMETYPE ] in which case
+// FTITLE is used as the value of `id' attribute in the item entries,
+// FILENAME is taken literally (i.e., not under Text/) and MIMETYPE is
+// used for that instead of application/xhtml+xml.
 // The unique identifier used will always be "BookId" and the chapters
 // will be stored under OEBPS/Text/.
-func prepareContentOpf(author, identifier, title string, files [][]string) string {
+func prepareContentOpf(author, identifier, title string, files [][]string) []byte {
 	var content strings.Builder
 
 	// Header.
-	content.WriteString(`<?xml version="1.0" encoding="utf-8">
+	content.WriteString(`<?xml version="1.0" encoding="utf-8"?>
 <package version="2.0" unique-identifier="BookId" xmlns="http://www.idpf.org/2007/opf">`)
 
 	// First do the metadata section.
@@ -214,47 +350,63 @@ func prepareContentOpf(author, identifier, title string, files [][]string) strin
 	content.WriteString("</dc:creator>\n")
 	content.WriteString("<dc:identifier id=\"BookId\">")
 	content.WriteString(identifier)
-	content.WriteString("</dc:identifer>\n")
-	content.WriteString("<dc:language>ta</dc:identifer>\n")
+	content.WriteString("</dc:identifier>\n")
+	content.WriteString("<dc:language>ta</dc:language>\n")
 	content.WriteString("<dc:title>")
 	content.WriteString(title)
 	content.WriteString("</dc:title>\n")
-	content.WriteString(`<dc:data opf:event="modification" xmlns:opf="http://www.idpf.org/2007/opf">`)
+	content.WriteString(`<dc:date opf:event="modification" xmlns:opf="http://www.idpf.org/2007/opf">`)
 	content.WriteString(time.Now().Format("2022-01-18"))
 	content.WriteString("</dc:date>\n")
 	content.WriteString("</metadata>\n\n")
 
 	// Manifest section.
 	content.WriteString("<manifest>")
-	for n, i := range files {
-		content.WriteString("\n<item id='Chapter")
-		content.WriteString(strconv.Itoa(n + 1))
-		content.WriteString("' href='Text/")
-		content.WriteString(i[1])
-		content.WriteString("' media-type='application/xhtml+xml'/>")
+	n := 1
+	for _, i := range files {
+		if len(i) != 3 {
+			content.WriteString("\n<item id='Chapter")
+			content.WriteString(strconv.Itoa(n))
+			content.WriteString("' href='Text/")
+			content.WriteString(i[1])
+			content.WriteString("' media-type='application/xhtml+xml'/>")
+			n += 1
+		} else {
+			content.WriteString("\n<item id='")
+			content.WriteString(i[0])
+			content.WriteString("' href='")
+			content.WriteString(i[1])
+			content.WriteString("' media-type='")
+			content.WriteString(i[2])
+			content.WriteString("' />")
+		}
 	}
 	content.WriteString("\n<item id='ncx' href='toc.ncx' media-type='application/x-dtbncx+xml'/>\n</manifest>\n")
 
 	// Spine section.
 	content.WriteString("\n<spine toc='ncx'>")
-	for n, _ := range files {
-		content.WriteString("\n<item idref='Chapter")
-		content.WriteString(strconv.Itoa(n + 1))
+	for i := 1; i <= n; i++ {
+		content.WriteString("\n<itemref idref='Chapter")
+		content.WriteString(strconv.Itoa(i))
 		content.WriteString("'/>")
 	}
 	content.WriteString("\n</spine>\n")
 
+	// No optional guide section.
 	content.WriteString("</package>\n")
-	return content.String()
+	return []byte(content.String())
 }
 
-func prepareTocNcx(author, identifer, title string, files [][]string) string {
+// Return the file contents of the toc.ncx file for the series.
+// Arguments have the same meaning as for prepareContentOpf.  FILES
+// with three elements (for content other than xhtml files) are simply
+// ignored.
+func prepareTocNcx(author, identifer, title string, files [][]string) []byte {
 	var content strings.Builder
 
 	// Header.
 	content.WriteString(`<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN"
-"http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
+<!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN" "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
 
 <ncx version="2005-1" xml:lang="ta" xmlns="http://www.daisy.org/z3986/2005/ncx/">
   <head>
@@ -274,12 +426,17 @@ func prepareTocNcx(author, identifer, title string, files [][]string) string {
 	content.WriteString(`</text></docAuthor>
   <navMap>`)
 
+	n := 1
 	// Now for the nested structure.
-	for n, i := range files {
+	for _, i := range files {
+		// Non-xhtml files.
+		if len(i) == 3 {
+			continue
+		}
 		content.WriteString("\n<navPoint id='Chapter")
-		content.WriteString(strconv.Itoa(n + 1))
+		content.WriteString(strconv.Itoa(n))
 		content.WriteString("' playOrder='")
-		content.WriteString(strconv.Itoa(n + 1))
+		content.WriteString(strconv.Itoa(n))
 		content.WriteString("'>\n")
 
 		content.WriteString("<navLabel><text>")
@@ -287,37 +444,141 @@ func prepareTocNcx(author, identifer, title string, files [][]string) string {
 		content.WriteString("</text></navLabel>\n")
 		content.WriteString("<content src='Text/")
 		content.WriteString(i[1])
-		content.WriteString("' />\n<navPoint>")
+		content.WriteString("' />\n</navPoint>")
+		n += 1
 	}
 
 	content.WriteString("\n</navMap>\n</ncx>\n")
-	return content.String()
+	return []byte(content.String())
 }
 
-func prepareContainerXml() string {
-	return `<?xml version="1.0" encoding="UTF-8"?>
+// Return the file contents of the container.xml file.
+func prepareContainerXml() []byte {
+	return []byte(`<?xml version="1.0" encoding="UTF-8"?>
 <container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
     <rootfiles>
         <rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>
    </rootfiles>
 </container>
-`
+`)
 }
 
-func prepareMimetype() string {
-	return "application/epub+zip\n"
+// Return the file contents of the mimetype file.
+func prepareMimetype() []byte {
+	return []byte("application/epub+zip\n")
+}
+
+// Return the slug part of the URL URL.
+func getSlug(url string) string {
+	u, err := nurl.Parse(url)
+	if err != nil {
+		panic(err)
+	}
+	s := strings.Split(u.Path, "/")
+	return s[len(s)-1]
+}
+
+// Get the series title of the story in URL URL.
+func getSeriesTitle(browser *rod.Browser, url string) string {
+	page := browser.MustPage(url)
+	page.MustWaitLoad()
+	defer page.Close()
+	return page.MustElement("h1.title").MustText()
+}
+
+type Zipfile struct {
+	Filename string
+	Content []byte
+}
+
+// Do everything for the book URL URL.
+func do(browser *rod.Browser, url string) {
+	slug := getSlug(url)
+	chapterLinks := extractLinks(slug)
+	// mimetype file must be the first one in the archive.
+	files := []Zipfile{
+		{"mimetype", prepareMimetype()},
+		{"META-INF/container.xml", prepareContainerXml()},
+	}
+	// [ FTITLE, FILENAME ] or [ FTITLE, FILENAME, MIMETYPE ]
+	var filesForEpub [][]string
+
+	fmt.Println("Total number of", len(chapterLinks), "chapters to fetch")
+
+	for n, ch := range chapterLinks {
+		fmt.Println("Fetching content for chapter", n+1, "via", ch[1])
+		content, extra := getContent(browser, ch, n+1)
+		if content == "" && extra == nil {
+			fmt.Println(ch[1], "does not have div .book-content, skipping...")
+			continue
+		}
+		fname := "Chapter" + strconv.Itoa(n+1) + ".xhtml"
+		files = append(files, Zipfile{
+			"OEBPS/Text/" + fname,
+			[]byte(content),
+		})
+		filesForEpub = append(filesForEpub, []string{
+			ch[0],
+			fname,
+		})
+		for _, i := range extra {
+			files = append(files, Zipfile{
+				"OEBPS/" + i.FileName,
+				i.FileContent,
+			})
+			filesForEpub = append(filesForEpub, []string{
+				i.FileId,
+				i.FileName,
+				i.Mimetype,
+			})
+		}
+	}
+
+	author := getAuthor(slug)
+	title := getSeriesTitle(browser, url)
+
+	fmt.Println("Creating content.opf and toc.ncx files")
+	files = append(files, Zipfile{
+		"OEBPS/content.opf",
+		prepareContentOpf(author, slug, title, filesForEpub),
+	})
+	files = append(files, Zipfile{
+		"OEBPS/toc.ncx",
+		prepareTocNcx(author, slug, title, filesForEpub),
+	})
+
+	fmt.Println("Creating the epub file")
+	epubFile, err := os.Create(slug + ".epub")
+	if err != nil {
+		panic(err)
+	}
+	defer epubFile.Close()
+
+	w := zip.NewWriter(epubFile)
+	defer w.Close()
+
+	for _, file := range files {
+		f, err := w.Create(file.Filename)
+		if err != nil {
+			panic(err)
+		}
+		_, err = f.Write(file.Content)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func main() {
-	// fmt.Println(getAuthor("vilaga-viduvena-amirthavarshini-by-sumathij-rajendran-cobmuv7jdcbr"))
-	// fmt.Println(getSeriesPart("vilaga-viduvena-amirthavarshini-by-sumathij-rajendran-cobmuv7jdcbr", 1, 1))
-	//fmt.Println(extractLinks("vilaga-viduvena-amirthavarshini-by-sumathij-rajendran-cobmuv7jdcbr"))
-	fmt.Println(prepareContentOpf("me", "myself", "lmao", [][]string{
-		[]string{"loooool", "chapter1.xhtml"},
-		[]string{"lmao", "chapter2.xhtml"},
-	}))
-	fmt.Println(prepareTocNcx("me", "myself", "lmao", [][]string{
-		[]string{"loooool", "chapter1.xhtml"},
-		[]string{"lmao", "chapter2.xhtml"},
-	}))
+	if (len(os.Args) == 1) {
+		fmt.Println("usage: pratilipi2epub LINKS...")
+		os.Exit(1)
+	}
+
+	browser := rod.New().MustConnect().NoDefaultDevice()
+	defer browser.MustClose()
+
+	for _, l := range os.Args[1:] {
+		do(browser, l)
+	}
 }
